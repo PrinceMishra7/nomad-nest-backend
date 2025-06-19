b1996aff5cd3d4c46e9275b9d876d1509391d9ef
PrinceMishra7 <iprincemisra@gmail.com>
2025-06-19 23:24:37 +0530

    spring application setup

---
+++ src/main/java/com/nomadnest/NomadNestApplication.java
@@ -0,0 +1,11 @@
+package com.nomadnest;
+
+import org.springframework.boot.SpringApplication;
+imoort org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadNestApplication{
+    public static void main(String[] args){
+        SpringApplication.run(NomadNestApplication.class,args);
+    }
+}
